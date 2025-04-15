0615e0d2750aaefc83fd517d11d629e95a2c3dfd
sangziii <sangziii@naver.com>
2025-04-16 01:10:51 +0900

    feat: meta 관련 설정 추가 - public/*: meta 관련 asset 추가 - constants/meta.ts: meta 상수 추가 - _app.tsx: meta 설정 적용

---
+++ src/constants/meta.ts
@@ -0,0 +1,15 @@
+export const META = {
+  THEME_COLOR: '#000000',
+  URL: 'https://kiabiz.kia.com',
+  COMMON: {
+    TITLE: '기아 비즈(Kia Biz)',
+    DESCRIPTION:
+      '기아 비즈는 기업을 위한 친환경 모빌리티 서비스로 차량부터 전용 App/Web까지 업무차량 토탈 솔루션을 제공합니다.',
+    KEYWORDS:
+      '모빌리티,구독서비스,차량구독,차량관리,업무용차량,법인차,관용차,전기차,FMS,스마트솔루션',
+    OG_URL: 'https://kiabiz.kia.com',
+  },
+  FAQ: {
+    TITLE: '서비스 도입 FAQ | 기아 비즈(Kia Biz) - 친환경 모빌리티 서비스',
+  },
+};
