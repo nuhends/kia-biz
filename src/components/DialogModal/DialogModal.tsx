7e5eaaf5d3b0e542ddc7b5df40c8fbd61839d037
sangziii <sangziii@naver.com>
2025-04-17 01:08:17 +0900

    feat: 다이얼로그 모달 및 이용약관 모달 컴포넌트 추가 - DialogModal 컴포넌트 생성: 다이얼로그 모달의 기본 구조 및 스타일 구현 - TermsModal 컴포넌트 생성: 이용약관 선택 기능 및 다이얼로그 모달 통합 - 이용약관 버전 선택을 위한 드롭다운 추가 및 내용 표시 기능 구현

---
+++ src/components/DialogModal/DialogModal.tsx
@@ -0,0 +1,52 @@
+import classNames from 'classnames';
+import { FC, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+import CloseIcon from '@/public/svgs/ic_close.svg';
+
+export interface DialogModalProps {
+  children: ReactNode;
+  isOpen: boolean;
+  title: string;
+  onClose: () => void;
+}
+
+const DialogModal: FC<DialogModalProps> = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
+
+  return createPortal(
+    // TODO: z-index 상수 관리
+    <div
+      className={classNames(
+        'min-w-[320px] max-w-[calc(100%-var(--side-padding)*2)] max-h-[calc(100%-var(--side-padding)*2)]',
+        'fixed inset-[0] z-105 flex items-center justify-center m-auto',
+      )}
+      role="dialog"
+    >
+      {/* dimmed layer */}
+      <div className="fixed inset-[0] bg-black/40" />
+      {/* content */}
+      <div className="relative flex flex-col w-full max-w-[960px] h-full bg-white">
+        <div className="h-full px-[20px] overflow-y-scroll xl:px-[40px]">
+          {/* 다이어로그 header */}
+          <div className="flex justify-between items-center h-[62px] pt-[4px] border-b-[2px] border-midnight-900">
+            <h4 className="text-[16px] font-bold">{title}</h4>
+            <button
+              aria-label="dialog 닫기"
+              className={classNames('p-[16px] [&>svg]:w-[24px] [&>svg]:h-[24px] mr-[-16px]')}
+              type="button"
+              onClick={onClose}
+            >
+              <CloseIcon />
+            </button>
+          </div>
+          {/* 다이어로그 body */}
+          <div className="overflow-y-auto pt-[12px] pb-[20px]">{children}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector('#__next') as HTMLElement,
+  );
+};
+
+export default DialogModal;
---
+++ src/screens/Faq/TermsModal.tsx
@@ -0,0 +1,39 @@
+import { FC, useState } from 'react';
+
+import { Term } from '@/src/api/terms';
+import DialogModal, { DialogModalProps } from '@/src/components/DialogModal/DialogModal';
+import { formatDate } from '@/src/utils/date';
+
+interface TermsModalProps extends Pick<DialogModalProps, 'isOpen' | 'onClose'> {
+  terms: Term[];
+}
+
+const TermsModal: FC<TermsModalProps> = ({ terms, ...rest }) => {
+  const [selectedVersion, setSelectedVersion] = useState<number>(terms[0]?.termsVersion || 0);
+
+  const selectedTermsHTML =
+    terms.find((term) => term.termsVersion === selectedVersion)?.contents || terms[0].contents;
+
+  const handleVersionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedVersion(Number(event.target.value));
+  };
+
+  return (
+    <DialogModal title="이용약관" {...rest}>
+      <div>
+        <div className="mb-[12px]">
+          <select className="w-full" value={selectedVersion} onChange={handleVersionChange}>
+            {terms.map(({ termsVersion, startDate, endDate }) => (
+              <option key={termsVersion} value={termsVersion}>
+                {`${formatDate(startDate)} ~ ${formatDate(endDate)}`}
+              </option>
+            ))}
+          </select>
+        </div>
+        {terms && <div id="termsFrame" dangerouslySetInnerHTML={{ __html: selectedTermsHTML }} />}
+      </div>
+    </DialogModal>
+  );
+};
+
+export default TermsModal;
