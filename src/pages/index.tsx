0090a52f8290f1c4705cf2f457f36b4e497e743f
sangziii <sangziii@naver.com>
2025-04-20 00:42:35 +0900

    fix: build 오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { getTerms } from '@/src/api/terms';
 import Footer from '@/src/components/Footer/Footer';
 import Header from '@/src/components/Header';
 import Layout from '@/src/components/Layout/Layout';
 import HomeScreen from '@/src/screens/Home/HomeScreen';
 
-import type { NextPageWithLayout } from '@/src/components/Layout/types';
 import type { Term } from '@/src/api/terms';
+import type { NextPageWithLayout } from '@/src/components/Layout/types';
+import type { GetServerSideProps } from 'next';
 
 interface Props {
   terms: Term[];
@@ -24,6 +26,16 @@ Home.getLayout = (page) => {
       <Footer terms={terms} />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const terms = await getTerms('JOIN_SERVICE_USE');
+
+  return {
+    props: {
+      terms,
+    },
+  };
 };
 
 export default Home;
